17d6b21e94bcab12baf3e4aeb9794e6cd5bd3327
zzh <zzhhuii@qq.com>
2017-10-27 17:19:02 +0800

    dd

--- mycms.sql
+++ mycms.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50714
 File Encoding         : 65001
 
-Date: 2017-10-26 17:12:33
+Date: 2017-10-27 17:18:22
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -48,6 +48,33 @@ INSERT INTO `mc_menu` VALUES ('20', '权限管理', 'admin', '', '1', '0', 'inde
 INSERT INTO `mc_menu` VALUES ('3', '菜单管理', 'admin', '', '1', '0', 'index/menu/index', '0', '', '', '0', '0');
 
 -- ----------------------------
+-- Table structure for mc_menu_new
+-- ----------------------------
+DROP TABLE IF EXISTS `mc_menu_new`;
+CREATE TABLE `mc_menu_new` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '父菜单id',
+  `type` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '菜单类型;1:有界面可访问菜单,2:无界面可访问菜单,0:只作为菜单',
+  `status` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '状态;1:显示,0:不显示',
+  `list_order` float NOT NULL DEFAULT '10000' COMMENT '排序',
+  `app` varchar(15) CHARACTER SET utf8 NOT NULL DEFAULT '' COMMENT '应用名',
+  `controller` varchar(30) CHARACTER SET utf8 NOT NULL DEFAULT '' COMMENT '控制器名',
+  `action` varchar(30) CHARACTER SET utf8 NOT NULL DEFAULT '' COMMENT '操作名称',
+  `param` varchar(50) CHARACTER SET utf8 NOT NULL DEFAULT '' COMMENT '额外参数',
+  `name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '菜单名称',
+  `icon` varchar(20) CHARACTER SET utf8 NOT NULL DEFAULT '' COMMENT '菜单图标',
+  `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '备注',
+  PRIMARY KEY (`id`),
+  KEY `status` (`status`),
+  KEY `parentid` (`parent_id`),
+  KEY `model` (`controller`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='后台菜单表';
+
+-- ----------------------------
+-- Records of mc_menu_new
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for mc_role
 -- ----------------------------
 DROP TABLE IF EXISTS `mc_role`;
@@ -65,15 +92,20 @@ CREATE TABLE `mc_role` (
   UNIQUE KEY `role_name_2` (`role_name`),
   KEY `parentId` (`pid`),
   KEY `status` (`status`)
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb4 COMMENT='角色表';
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4 COMMENT='角色表';
 
 -- ----------------------------
 -- Records of mc_role
 -- ----------------------------
-INSERT INTO `mc_role` VALUES ('1', '0', '1', '1509004631', '0', '0', '6', '');
-INSERT INTO `mc_role` VALUES ('4', '0', '1', '1509004921', '0', '0', '6r', '');
+INSERT INTO `mc_role` VALUES ('1', '0', '0', '1509004631', '1509067238', '0', '6', '');
+INSERT INTO `mc_role` VALUES ('4', '0', '0', '1509004921', '1509067311', '0', '6r', '');
 INSERT INTO `mc_role` VALUES ('42', '0', '1', '1509008514', '0', '0', '65', '');
 INSERT INTO `mc_role` VALUES ('43', '0', '1', '1509008532', '0', '0', '55', '');
+INSERT INTO `mc_role` VALUES ('44', '0', '1', '1509066071', '0', '0', '77', '');
+INSERT INTO `mc_role` VALUES ('46', '0', '1', '1509066228', '0', '0', '775', '');
+INSERT INTO `mc_role` VALUES ('47', '0', '1', '1509066277', '0', '0', '56', '');
+INSERT INTO `mc_role` VALUES ('48', '0', '1', '1509066322', '0', '0', '34', '');
+INSERT INTO `mc_role` VALUES ('50', '0', '1', '1509066360', '0', '0', '345', '');
 
 -- ----------------------------
 -- Table structure for mc_role_user
@@ -122,9 +154,10 @@ CREATE TABLE `mc_user` (
   UNIQUE KEY `id` (`id`),
   KEY `user_login` (`user_name`),
   KEY `user_nickname` (`user_nickname`)
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COMMENT='用户表';
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COMMENT='用户表';
 
 -- ----------------------------
 -- Records of mc_user
 -- ----------------------------
 INSERT INTO `mc_user` VALUES ('34', '1', '0', '0', '0', '0', '0', '1508811133', '1', 'admin', '21232f297a57a5a743894a0e4a801fc3', '超级管理员', '', '', '', '', '', '', '', null);
+INSERT INTO `mc_user` VALUES ('35', '1', '0', '0', '0', '0', '0', '1509088849', '1', 'zzh', '6b5d1372c0d805e15c5504323f93fdc9', '', '', '', '', '', '', '', '', null);
