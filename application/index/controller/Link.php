53ce015bc67f0f47684d666179a8d6fb037ca1ab
zzh <zzhhuii@qq.com>
2017-11-14 11:23:33 +0800

    delete runtime' ;

--- application/index/controller/Link.php
+++ application/index/controller/Link.php
@@ -18,7 +18,7 @@ class Link extends Baseinit {
 		return $this->fetch("link/edit");
 	}
 	/**
-	 * 添加超链
+	 * 新添加超链
 	 * @author 钟朝辉 <zzhhuii@qq.com>
 	 * @date   2017-11-08T14:48:21+0800
 	 */
@@ -33,7 +33,7 @@ class Link extends Baseinit {
 				exit;
 			}
 			if ($res) {
-				return $this->success('ok', url("Link/index"));
+				return $this->success('添加成功', url("Link/index"));
 			} else {
 				return $this->error("数据库繁忙！");
 			}
@@ -47,7 +47,34 @@ class Link extends Baseinit {
 	 * @date   2017-11-08T16:55:49+0800
 	 */
 	public function edit($id = 0) {
-
+		$id = (int) $id;
+		$info = DB::name('link')->where("id='$id'")->find();
+		$this->assign('info', $info);
+		return $this->fetch('Link/edit');
+	}
+	/**
+	 * 编辑超链入库
+	 * @author 钟朝辉 <zzhhuii@qq.com>
+	 * @date   2017-11-08T14:48:21+0800
+	 */
+	public function editPost() {
+		if (input('post.')) {
+			$data = input('post.');
+			$id = $data['id'];
+			try {
+				$res = Db::name('link')->where("id='$id'")->update($data);
+			} catch (\Exception $e) {
+				return $this->error($e, 'system_error', $data);
+				exit;
+			}
+			if ($res) {
+				return $this->success('修改成功', url("Link/index"));
+			} else {
+				return $this->error("数据库繁忙！");
+			}
+		} else {
+			return $this->error("数据为空！");
+		}
 	}
 	public function del() {
 
